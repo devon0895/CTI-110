ed7d83c78acf11d8a789117851275aea8a7ac4e6
devon0895 <160151014+devon0895@users.noreply.github.com>
2024-02-20 17:00:14 +0000

    new hw2 assignment

---
+++ P1HW2_PooleDevon.py
@@ -0,0 +1,27 @@
+#DevonPoole
+#2/20/24
+#P1HW2
+#Doing basic math in python
+
+exp = (int(gas + lodge + food))
+
+budget = int(input("What is your travel budget?", ))
+print("Your vacation budget is:", budget)
+
+area = input("Where is your vacation destination?", )
+print("You're going to", area)
+
+gas = int(input("How much will you spend on gas?", ))
+print("You'll spend", "$", gas, "in gas money")
+
+lodge = int(input("How much will you spend on the hotel room?"))
+print("The sum of hotel expenses add up to", lodge )
+
+food = int(input("How much do you plan to spend on food?" ))
+print("Your food budget adds up to", food )
+
+print("Your vacation expenses add up to", area + gas + lodge + food )
+
+print(budget -)
+
+print("Based on your budget and projected expenses, you'll have", budget - totalexp, "left after the trip.")
